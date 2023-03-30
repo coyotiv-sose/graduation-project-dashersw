58b3b2270b86257d585dfecc82cf162779cd6571
Armagan Amcalar <armagan@amcalar.com>
2023-03-30 13:46:58 +0000

    Implement recent API changes in index.js and convert all previous method call examples to axios calls. Also use baseURL configuration to simplify axios calls.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const User = require('./user')
 
 const axios = require('axios')
 
+axios.defaults.baseURL = 'http://localhost:3000'
+
 console.log('Picnigram is a social network for making picnics fun again!')
 
 // I need two main objects, Users and Picnics, and Users can create Picnics and other Users can join them
@@ -18,56 +20,71 @@ console.log('Picnigram is a social network for making picnics fun again!')
 
 // fetch users with axios
 
-// axios.get('http://localhost:3000/users').then(response => {
+// axios.get('/users').then(response => {
 //   console.log(response.data)
 // })
 
 // create a user with axios
 
 async function main() {
-  await axios.post('http://localhost:3000/users', {
+  await axios.post('/users', {
     name: 'Armagan',
     hacked: true,
   })
 
-  await axios.post('http://localhost:3000/users', {
+  await axios.post('/users', {
     name: 'Numan',
   })
 
-  await axios.post('http://localhost:3000/users/Armagan/picnics', {
-    name: 'ArmaganBirthdayPicnic',
+  await axios.post('/picnics', {
+    user: 'Armagan',
+    name: "Armagan's Birthday Picnic",
     location: 'Tempelhofer Feld',
     date: '2023-05-01',
   })
 
-  await axios.post('http://localhost:3000/users/Numan/picnics', {
+  await axios.post('/picnics', {
+    user: 'Numan',
     name: "Numan's Counter-Picnic",
     location: 'Hasenheide',
     date: '2023-05-02',
   })
 
-  await axios.post('http://localhost:3000/users/Armagan/picnics/ArmaganBirthdayPicnic/attendees', {
+  await axios.post("/picnics/Armagan's Birthday Picnic/attendees", {
     name: 'Numan',
   })
 
-  const allUsers = await axios.get('http://localhost:3000/users')
+  await axios.post("/picnics/Numan's Counter-Picnic/attendees", {
+    name: 'Armagan',
+  })
+
+  await axios.put("/picnics/Numan's Counter-Picnic/items", {
+    user: 'Armagan',
+    name: 'beer',
+    quantity: 6,
+  })
+
+  await axios.put("/picnics/Numan's Counter-Picnic/items", {
+    user: 'Numan',
+    name: 'beer',
+    quantity: 3,
+    desiredQuantity: 12,
+  })
+
+  await axios.put("/picnics/Numan's Counter-Picnic/items", {
+    user: 'Numan',
+    name: 'tent',
+    quantity: 1,
+  })
+
+  await axios.put("/picnics/Numan's Counter-Picnic/items", {
+    user: 'Numan',
+    name: 'wine',
+    quantity: 2,
+  })
+
+  const allUsers = await axios.get('/users')
   console.log('List of all users', allUsers.data)
 }
 
 main()
-
-// const armagansPicnic = armagan.createPicnic(`Armagan's Birthday Picnic`, 'Tempelhofer Feld', '2023-05-01')
-// const numansPicnic = numan.createPicnic(`Numan's Counter-Picnic`, 'Hasenheide', '2023-05-02')
-
-// armagan.joinPicnic(numansPicnic)
-// numan.joinPicnic(armagansPicnic)
-
-// armagan.bringItem('beer', 6, numansPicnic)
-// numan.bringItem('beer', 3, numansPicnic)
-// numan.bringItem('tent', 1, numansPicnic)
-// numan.bringItem('wine', 2, numansPicnic)
-
-// numansPicnic.items[0].desiredQuantity = 12
-// console.log(numansPicnic.details)
-// // console.log(armagan.profile)
-// // console.log(numan.profile)
