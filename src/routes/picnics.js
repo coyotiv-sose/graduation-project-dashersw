f02bfb873e2918a0e4686d38d02c7695028ea5c7
Armagan Amcalar <armagan@amcalar.com>
2023-03-30 12:58:08 +0000

    Implement picnics global listing with both JSON and HTML

--- src/routes/picnics.js
+++ src/routes/picnics.js
@@ -2,23 +2,27 @@ var express = require('express')
 const Picnic = require('../picnic')
 var router = express.Router()
 
-/* GET users listing. */
+/* GET picnics listing. */
 router.get('/', function (req, res, next) {
-  res.send(Picnic.list)
-})
+  if (req.query.view === 'json')
+    return res.send(
+      Picnic.list.map(picnic => ({
+        name: picnic.name,
+        location: picnic.location,
+        date: picnic.date,
+        attendees: picnic.attendees.map(attendee => attendee.name),
+        items: picnic.items.map(item => ({
+          name: item.name,
+          quantity: item.quantity,
+          desiredQuantity: item.desiredQuantity,
+          isEnough: item.isEnough,
+        })),
+      }))
+    )
 
-// join a picnic
-router.post('/:picnicId/attendees', function (req, res, next) {
-  try {
-    const picnic = Picnic.list.find(picnic => picnic.name === req.params.picnicId)
-    const user = User.list.find(user => user.name === req.body.userId)
-
-    user.joinPicnic(picnic)
-
-    res.send({ name: picnic.name, location: picnic.location, date: picnic.date })
-  } catch (e) {
-    res.status(400).send(e.message)
-  }
+  res.render('picnics', {
+    picnics: Picnic.list,
+  })
 })
 
 module.exports = router
