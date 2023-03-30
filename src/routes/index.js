b03b5cd126827dee8df6f9fbd0a8a58c1260625b
Armagan Amcalar <armagan@amcalar.com>
2023-03-30 12:48:37 +0000

    Render index pug in index router, and add Log in and Sign up links (non-functional)

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('users', { title: 'Picnigram app' })
+  res.render('index', { title: 'Picnigram app' })
 })
 
 module.exports = router
