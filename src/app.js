9310b08e309b3b2fb98549e106082a030b933f3e
Armagan Amcalar <armagan@amcalar.com>
2023-03-30 13:44:42 +0000

    Add pluralize library to app.locals so it can be used from within pug templates

--- src/app.js
+++ src/app.js
@@ -3,12 +3,15 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const pluralize = require('pluralize')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 const picnicsRouter = require('./routes/picnics')
 
 const app = express()
+
+app.locals.pluralize = pluralize
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
