5edd7bbb3e0d9c9358297b5450da2dcad0155c88
Armagan Amcalar <armagan@amcalar.com>
2023-04-04 13:12:51 +0000

    Add database-connection.js file

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
+require('dotenv').config()
+
 const createError = require('http-errors')
 const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const pluralize = require('pluralize')
-require('dotenv').config()
+
+require('./database-connection')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
