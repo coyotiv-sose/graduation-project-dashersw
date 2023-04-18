248f650494f169f8797e2d8f726d235361ad7cb1
Armagan Amcalar <armagan@amcalar.com>
2023-04-18 15:56:09 +0000

    Add example counter components for options and composition API

---
+++ frontend/src/components/Counter.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  name: {
+    type: String,
+    required: true
+  }
+})
+
+const count = ref(0)
+
+const increment = () => {
+  count.value++
+}
+
+const decrement = () => {
+  count.value--
+}
+</script>
+
+<template lang="pug">
+div
+  h1 {{ name }}
+  p Count: {{ count }}
+  button(@click='increment') Increment
+  button(@click='decrement') Decrement
+</template>
---
+++ frontend/src/components/CounterOptionsApi.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  name: 'Counter',
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    },
+    decrement() {
+      this.count--
+    }
+  }
+}
+</script>
+
+<template lang="pug">
+div
+  h1 {{ name }}
+  p Count: {{ count }}
+  button(@click='increment') Increment
+  button(@click='decrement') Decrement
+</template>
