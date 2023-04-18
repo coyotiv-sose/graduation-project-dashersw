bce4b845a9a19b235186ce5b216e828da3d3abf9
Armagan Amcalar <armagan@amcalar.com>
2023-04-18 15:55:46 +0000

    Remove navigation from App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,16 +5,12 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+    <!-- <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
-    </div>
+    </div> -->
   </header>
   <h1>Picnigram</h1>
   <Suspense>
